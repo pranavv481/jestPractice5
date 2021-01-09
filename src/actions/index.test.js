1a9fd9aa95dd8c0d875f51b826ed69014a4f4d30
pranavv481 <pranavv481@gmail.com>
2021-01-10 00:20:43 +0530

    first commit

---
+++ src/actions/index.test.js
@@ -0,0 +1,9 @@
+import {actionTypes} from "./index";
+
+describe('correctGuess', ()=>{
+    test('action with type `CORRECT_GUESS`', ()=>{
+        // const action = correctGuess();
+        // expect(action).toEqual({type:actionTypes.CORRECT_GUESS})
+        
+    })
+})
